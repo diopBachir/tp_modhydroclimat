9bfd2be866a4ed0e97d76e9e254352a2adb48f4f
diopBachir <09.bachir.diop.10@mail.com>
2026-01-27 06:50:18 +0100

    Add need packages

---
+++ prepare_climdata.R
@@ -0,0 +1,36 @@
+files <- list.files("data/CNRM", pattern = "^cnrm_.*\\.csv$", full.names = T)
+
+for (f in files) {
+
+  df <- read.csv(f, header = FALSE)
+  colnames(df) <- c("year", "month", "day", "value")
+
+  date <- sprintf("%04d%02d%02d", df$year, df$month, df$day)
+  out <- data.frame(date, df$value)
+
+  write.table(out,
+              file = f,
+              sep = ",",
+              row.names = FALSE,
+              col.names = FALSE,
+              quote = FALSE)
+}
+
+
+files <- list.files("data/SMHI-MPI", pattern = "^smhi_mpi_.*\\.csv$", full.names = T)
+
+for (f in files[1]) {
+
+  df <- read.csv(f, header = FALSE)
+  colnames(df) <- c("year", "month", "day", "value")
+
+  date <- sprintf("%04d%02d%02d", df$year, df$month, df$day)
+  out <- data.frame(date, df$value)
+
+  write.table(out,
+              file = f,
+              sep = ",",
+              row.names = FALSE,
+              col.names = FALSE,
+              quote = FALSE)
+}
